5061217e53c0249b76b5aa6c81b08cbc1685ce58
pavelperkin <perkinpv@mail.ru>
2014-05-16 11:42:18 +0300

    Initialize Ruby on Rails applcation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studout::Application.config.secret_key_base = '53a8d7c2e5e7909f2ac0e0a77c8c510200f373761ca2e644709b9757cea51d09b29d9bbadc230c7568af29049710b85401a0de0c98a75bf7715a6961fb458494'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Studout::Application.config.session_store :cookie_store, key: '_studout_session'
