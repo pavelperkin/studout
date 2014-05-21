3b53c7bf2a20ae6767ca13733c88fed2a9b7dbb3
pavelperkin <perkinpv@mail.ru>
2014-05-21 13:24:57 +0300

    use google maps api

--- app/models/place.rb
+++ app/models/place.rb
@@ -11,19 +11,16 @@ class Place < ActiveRecord::Base
 
   private
 
-
-
   def set_lat_and_lng
-    response = HTTParty.get(URI.encode("http://geocode-maps.yandex.ru/1.x/?geocode=#{get_address}&format=json"))
-    self.longitude, self.latitude = response["response"]["GeoObjectCollection"]["featureMember"]
-                                      .first["GeoObject"]["Point"]["pos"].split
+    response = HTTParty.get(URI.encode("http://maps.googleapis.com/maps/api/geocode/json?address=#{get_address}&sensor=_false"))
+    coordinates = response["results"].first["geometry"]["location"]
+    self.latitude = coordinates["lat"]
+    self.longitude = coordinates["lng"]
   end
 
 
   def get_address
-    [self.city.country.name,
-     self.city.name,
-     self.address.split.join(",")].join(",")
+    [self.address.split.join("+"), self.city.name, self.city.country.name].join(",")
   end
 
 end
